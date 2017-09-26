7d33197520cd78490a851bdece634a4b328c0a1d
tadahiro.wada <tadahiro.wada@d8aspring.com>
2017-09-26 21:04:08 +0900

    initial commit

---
+++ src/main/kotlin/com/github/wtadahiro/kotlintodo/Greeting.kt
@@ -0,0 +1,3 @@
+package com.github.wtadahiro.kotlintodo
+
+data class Greeting(val id: Long, val content: String)
---
+++ src/main/kotlin/com/github/wtadahiro/kotlintodo/GreetingController.kt
@@ -0,0 +1,15 @@
+package com.github.wtadahiro.kotlintodo
+
+import org.springframework.web.bind.annotation.*
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class GreetingController {
+
+    val counter = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            Greeting(counter.incrementAndGet(), "Hello, $name")
+
+}
